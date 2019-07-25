b1f10b8b178e53c2c6281bd015ed5fcfd1da8cbb
Simonwtaylor <Simonwtaylor93@gmail.com>
2019-07-25 18:59:10 +0100

    Added new heroku repo

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
 import logger from 'redux-logger';
 
-const middlewares = [logger];
+const middlewares: any[] = [];
+
+if(process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
