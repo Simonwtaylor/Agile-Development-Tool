10b217ea8046d05bd143da8ba86c57b3d6c2ae61
Simonwtaylor <Simonwtaylor93@gmail.com>
2019-08-19 12:46:03 +0100

    Updates to new boards

--- src/components/boards/boards.component.tsx
+++ src/components/boards/boards.component.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Grid, Card } from 'semantic-ui-react';
+import { Grid, Card, Form } from 'semantic-ui-react';
 import BoardColumn from '../board-column/board-column.component';
 import CustomButton from '../custom-button/custom-button.component';
+import './boards.style.scss';
 
 export interface IBoardsProps {
   boards: any[];
@@ -10,6 +11,15 @@ export interface IBoardsProps {
 const Boards: React.FC<IBoardsProps> = ({
   boards
 }) => {
+
+  // Hooks
+  const [newBoard, setNewBoard] = React.useState(false);
+  const [boardName, setBoardName] = React.useState('');
+
+  const handleBoardNameChange = (e: any) => {
+    setBoardName(e.target.value);
+  };
+
   return (
     <>
     {
@@ -28,18 +38,48 @@ const Boards: React.FC<IBoardsProps> = ({
         )
       })
     }
-    <div className="col card">
-      <div className="card-body card-container">
-        <h3>Add New Board</h3>
-        <CustomButton 
-          inverted={true}
-          color={'green'}
-          onClick={() => console.log('NEW BOARD')}
-        >
-          <span role="img" aria-label="save">➕</span> Add New Board
-        </CustomButton>
-      </div>
-    </div> 
+    <Grid.Column key={'addnewboard'}>
+      <Card className={'board'}>
+        <div className="col card">
+          <div className="card-body card-container">
+            {(!newBoard && 
+            <>
+              <h3>Add New Board</h3>
+              <CustomButton 
+                inverted={true}
+                color={'green'}
+                onClick={() => setNewBoard(!newBoard)}
+              >
+                <span role="img" aria-label="save">➕</span> Add New Board
+              </CustomButton>
+            </>
+            )}
+            {(newBoard && 
+            <>
+              <Form>
+                <Form.Field>
+                  <label>Name</label>
+                  <input 
+                    placeholder='Name...'
+                    value={boardName}
+                    name={'name'}
+                    onChange={handleBoardNameChange}
+                  />
+                </Form.Field>
+                <CustomButton 
+                  inverted={true}
+                  color={'green'}
+                  onClick={() => setNewBoard(!newBoard)}
+                >
+                  <span role="img" aria-label="save">💾</span> Save New Board
+                </CustomButton>
+              </Form>
+            </>
+            )}
+          </div>
+        </div>
+      </Card>
+    </Grid.Column>
     </>
   );
 }
